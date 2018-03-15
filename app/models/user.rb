05badfcf74e18a491c2f4efcd67e55e3a13c561c
liujohnson118 <liujohnson118@yahoo.com>
2018-03-15 05:15:01 +0000

    user can be added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, uniqueness: { case_sensitive: false }
+  validate :validateEmail
+  validates :phone, numericality: {only_integer: true, minimum: 0, maximum: 9999999999999}
+  validates :age, numericality: {only_integer: true, minimum: 0, maximum: 200}
+  def validateEmail
+    email_check = email.include?("@") && email.split("@")[1].include?(".")
+    if email_check == false
+      errors.add(:email, 'Invalid email address')
+    end
+    email_check
+  end
 end
